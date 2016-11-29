93931016e09604e2c7f390c9fe924942fe4bf896
Robalexg <alexg7070@yahoo.com>
2016-11-29 15:32:36 -0600

    Addded user input

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const prompt = require('prompt')
 
-var postions = ['1','2','3','4','5','6','7','8','9']
+var postions = [1,2,3,4,5,6,7,8,9]
 
 var a = "1";
 var b = "2";
@@ -14,14 +14,35 @@ var g = "7";
 var h = "8";
 var i = "9";
 
-var board = "=======" + "\n" + a + "|" + b + "|" + c +"\n" +
+
+
+function getBoard(){
+  var board = "=======" + "\n" + a + "|" + b + "|" + c +"\n" +
     d + "|" + e + "|" + f + "\n" +
     g + "|" +  h + "|" + i + "\n" + "=======";
+  return board
+}
+
+
+prompt.start()
 
 console.log("Tic Tac Toeeeeeee!!!!!!!!" + '\n' + "----------")
-console.log("Enter your name: ")
-prompt.get(['name'],function(err,result){
-  
+console.log("Enter the players Names: ")
+prompt.get(['Player_1'],function(err,player1){
+    prompt.get(['Player_2'],function (err,player2) {
+      var p1 = player1.Player_1
+      var p2 = player2.Player_2
+      console.log("Welcome, " + p1 + " and " + p2)
+      console.log(getBoard())
+      console.log(p1+ " is X." + '\n' + p2+ ' is O')
+      console.log(p1 + ', Please enter a number' )
+      prompt.get(['num'],function(err, num){
+        if(postions.indexOf(num.num)){
+          a = 'chyea'
+          console.log(getBoard())
+        }
+      })
+    })
 })
 
 app.listen(3000)
